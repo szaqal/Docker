85a65fde0649c3b483641f14b22dfaecf6ace5e8
szaqal <malczyk.pawel@gmail.com>
2020-04-14 13:52:13 +0200

    Work

--- old/dummy-maven-project/generate.sh
+++ old/dummy-maven-project/generate.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-rm -fr dummy/
+#rm -fr dummy/
+
+if [[ -z "${MVN_VERSION}" ]]; then
+	echo "Version not provided"
+	exit 1
+fi
+
+if [[ -z "${MVN_ARTIFACT}" ]]; then
+	echo "Artifact not provided"
+	exit 1
+fi 
+
+if [[ -z "${MVN_GROUP}" ]]; then
+	echo "GROUP ID not provided"
+	exit 1
+fi
 
 mvn archetype:generate \
-	-Dversion=1.0.0-SNAPSHOT \
+	-Dversion=${MVN_VERSION} \
 	-DinteractiveMode=false  \
-	-DartifactId=dummy \
-	-DgroupId=com.dummy \
+	-DartifactId=${MVN_ARTIFACT} \
+	-DgroupId=${MVN_GROUP} \
 	-DarchetypeGroupId=org.apache.maven.archetypes \
 	-DarchetypeArtifactId=maven-archetype-quickstart \
 	-DarchetypeVersion=1.4
