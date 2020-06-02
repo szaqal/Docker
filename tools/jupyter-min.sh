a7110858c547ecd066bff86b70ced2a4e275d024
szaqal <malczyk.pawel@gmail.com>
2020-06-02 09:40:57 +0200

    Scipy

--- tools/jupyter-min.sh
+++ tools/jupyter-min.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8888:8888 jupyter/minimal-notebook
+docker run --name "jupyter" -p 8888:8888 jupyter/minimal-notebook
---
+++ tools/jupyter-scipy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --name "jupyter" -p 8888:8888 jupyter/scipy-notebook
+
